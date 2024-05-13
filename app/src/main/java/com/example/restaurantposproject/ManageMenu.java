bdab409e97b870dbe59f1ad5201d4eea61153b58
rober <robert_abou_nader@live.com>
2024-05-13 21:39:38 +0300

    working add remove modify menu item

--- app/src/main/java/com/example/restaurantposproject/ManageMenu.java
+++ app/src/main/java/com/example/restaurantposproject/ManageMenu.java
@@ -1,6 +1,9 @@
 package com.example.restaurantposproject;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -14,6 +17,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class ManageMenu extends AppCompatActivity {
 ImageButton back;
 AppCompatButton add,delete,modify,selectImage;
@@ -21,6 +31,7 @@ AppCompatButton add,delete,modify,selectImage;
 EditText itemName,price,description;
 
 Spinner category;
+FirebaseDatabase database = FirebaseDatabase.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,7 +75,13 @@ Spinner category;
                     Toast.makeText(ManageMenu.this, "Please select an image", Toast.LENGTH_SHORT).show();
                     Toast.makeText(ManageMenu.this, "Please select a category", Toast.LENGTH_SHORT).show();
                 } else {
-                    // Add item to menu
+                    DatabaseReference myRef = database.getReference("food_list").child(itemCategory);
+                    String id = myRef.push().getKey();
+                    double price = Double.parseDouble(itemPrice);
+                    FoodItem foodItem= new FoodItem(item, itemDescription, price, "itemImage");
+                    myRef.child(id).setValue(foodItem);
+                    Toast.makeText(ManageMenu.this, "Item added successfully", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
@@ -72,21 +89,82 @@ Spinner category;
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Delete item from menu
+                String itemCategory = category.getSelectedItem().toString();
+                String item = itemName.getText().toString();
+
+
+                DatabaseReference myRef = database.getReference("food_list").child(itemCategory);
+
+                Query itemQuery = myRef.orderByChild("name").equalTo(item);
+
+                ((Query) itemQuery).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot itemSnapshot: dataSnapshot.getChildren()) {
+                            itemSnapshot.getRef().removeValue();
+
+                        }
+                        if(dataSnapshot.getChildrenCount() == 0){
+                            Toast.makeText(ManageMenu.this, "Item not found", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ManageMenu.this, "Item deleted successfully", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled", databaseError.toException());
+                    }
+                });
             }
         });
 
         modify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Modify item in menu
+                String itemCategory = category.getSelectedItem().toString();
+                String item = itemName.getText().toString();
+                String itemPrice = price.getText().toString();
+                String itemDescription = description.getText().toString();
+
+                DatabaseReference myRef = database.getReference("food_list").child(itemCategory);
+
+                Query itemQuery = myRef.orderByChild("name").equalTo(item);
+
+                itemQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot itemSnapshot: dataSnapshot.getChildren()) {
+                                try {
+                                    double price = Double.parseDouble(itemPrice);
+                                    FoodItem foodItem= new FoodItem(item, itemDescription, price, "itemImage");
+                                    itemSnapshot.getRef().setValue(foodItem);
+                                    Toast.makeText(ManageMenu.this, "Item modified successfully", Toast.LENGTH_SHORT).show();
+                                } catch (NumberFormatException e) {
+                                    Toast.makeText(ManageMenu.this, "Invalid price format", Toast.LENGTH_SHORT).show();
+                                } catch (Exception e) {
+                                    Toast.makeText(ManageMenu.this, "Error modifying item", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        } else {
+                            Toast.makeText(ManageMenu.this, "Item not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled", databaseError.toException());
+                    }
+                });
             }
         });
 
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Select image for item
+
             }
         });
 
